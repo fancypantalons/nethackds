df6691c5bcc8718eda5cf989db019bbed7234816
Brett Kosinski <fancypantalons@gmail.com>
2008-10-18 15:44:20 +0000

    Moved things around to reduce the odds of a conflict.
    git-svn-id: svn+ssh://frodo/home/brettk/svn/nethackds/branches/vanilla@494 abe77b40-7291-486f-ab47-e188472a22da

--- include/wintype.h
+++ include/wintype.h
@@ -47,10 +47,6 @@ typedef struct mi {
 #define ATR_BLINK   5
 #define ATR_INVERSE 7
 
-#ifdef NDS
-#  define ATR_NOREFLOW 0x1000
-#endif
-
 /* nh_poskey() modifier types */
 #define CLICK_1     1
 #define CLICK_2     2
@@ -71,5 +67,8 @@ typedef struct mi {
 #define MENU_INVERT_PAGE	'~'
 #define MENU_SEARCH		':'
 
+#ifdef NDS
+#  define ATR_NOREFLOW 0x1000
+#endif
 
 #endif /* WINTYPE_H */

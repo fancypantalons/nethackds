c6d9181ad842c4f293505e13bd035127c3ca06df
Brett Kosinski <brettk@testtrack.b-ark.ca>
2018-05-09 17:28:37 -0600

    Updated to latest ARM7 reference code.
    The latest version of libnds requires a call to touchInit.  Without
    that, well I'll be darned, the touchscreen doesn't work.  So, that's
    fixed, plus sound is initialized properly, so bonus.

--- sys/nds/arm7/src/arm7_main.c
+++ sys/nds/arm7/src/arm7_main.c
@@ -54,12 +54,21 @@ void powerButtonCB() {
 //---------------------------------------------------------------------------------
 int main() {
 //---------------------------------------------------------------------------------
+	// clear sound registers
+	dmaFillWords(0, (void*)0x04000400, 0x100);
+
+	REG_SOUNDCNT |= SOUND_ENABLE;
+	writePowerManagement(PM_CONTROL_REG, ( readPowerManagement(PM_CONTROL_REG) & ~PM_SOUND_MUTE ) | PM_SOUND_AMP );
+	powerOn(POWER_SOUND);
+
 	readUserSettings();
+	ledBlink(0);
 
 	irqInit();
 	// Start the RTC tracking IRQ
 	initClockIRQ();
 	fifoInit();
+	touchInit();
 
 	SetYtrigger(80);
 
@@ -72,8 +81,8 @@ int main() {
 	irqSet(IRQ_VBLANK, VblankHandler);
 
 	irqEnable( IRQ_VBLANK | IRQ_VCOUNT | IRQ_NETWORK);
-	
-	setPowerButtonCB(powerButtonCB);   
+
+	setPowerButtonCB(powerButtonCB);
 
 	// Keep the ARM7 mostly idle
 	while (!exitflag) {
